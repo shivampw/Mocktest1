2692778cb6b6f6bdab76093e2cc6f4115a78637b
shivampw <123968519+shivampw@users.noreply.github.com>
2023-06-27 18:01:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int left = 0; // Pointer to track the position for non-zero elements
+
+        // Iterate through the array
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                // If the current element is non-zero, swap it with the left pointer
+                int temp = nums[i];
+                nums[i] = nums[left];
+                nums[left] = temp;
+
+                left++; // Increment the left pointer
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        System.out.print("Input: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        moveZeroes(nums);
+
+        System.out.print("\nOutput: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
